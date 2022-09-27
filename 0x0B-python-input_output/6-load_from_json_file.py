a9c1df1129b153d95ce30158a785b121dd004e5e
khendimike <khendimike@gmail.com>
2022-09-26 23:01:44 -0700

    Create object from a JSON file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Module defines a fxn creates an object from a JSON file."""
+import json
+
+
+def load_from_json_file(filename):
+    """Create an object from a JSON file."""
+    with open(filename) as f:
+        return json.load(f)
