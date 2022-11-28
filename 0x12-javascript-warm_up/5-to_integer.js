be72452696a019151205ef683b1b8ce2439eb400
khendimike <khendimike@gmail.com>
2022-11-28 18:57:05 +0000

    An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-if (Number(process.argv[2])) {
-   console.log('My number: ' + Number(process.argv[2]));
-} else {
+if (isNaN(process.argv[2])) {
   console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(process.argv[2]));
 }
