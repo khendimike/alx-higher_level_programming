97782bf1966114f935a1bedb9f15961eed0ed1f4
khendimike <khendimike@gmail.com>
2022-11-28 19:30:47 +0000

    script that prints x C is Cfuntimes

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
-/* prints x C is 
-Where x is the first argument of the script
-If the first argument t be converted to an integer,
-Missing number of 
-*/
 
-const lang = 'C is fun';
-
-if (isNaN(process.argv[2])) {
-  console.log('Missing number of occurrences');
+let printXt = parseInt(process.argv[2]);
+if (isNaN(printXt) || process.argv[2] === undefined) {
+  console.log('Missing number of occurences');
 } else {
-  for (let i = 0; i < parseInt(process.argv[2]); i++) {
-    console.log(lang);
+  while (printXt > 0) {
+    console.log('C is fun');
+    printXt--;
   }
-} 
+}
